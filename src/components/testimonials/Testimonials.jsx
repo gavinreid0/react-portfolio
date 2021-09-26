e6ed441afed1b3638bb0e97f0e02c19323a059c0
Gavin Reid <gavinreid94@gmail.com>
2021-09-25 22:44:08 -0500

    added dropdown menu (not responsive yet)

--- src/components/testimonials/Testimonials.jsx
+++
@@ -1,9 +0,0 @@
-import "./testimonials.css"
-
-export default function Testimonials() {
-    return (
-        <div className="testimonials" id="testimonials">
-            
-        </div>
-    )
-}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,11 +2,14 @@ import "./topbar.css";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import EmailIcon from '@mui/icons-material/Email';
 
-export default function Topbar({ menuOpen, setMenuOpen }) {
+
+
+
+export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
-        <div className="left">
+        <div className="topbar-left">
           <a href="#intro" className="logo">
             Portfolio.
           </a>
@@ -20,12 +23,34 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </div>
         </div>
         <div className="topbar-right">
-          <div className="hamburger">
+          <div className="hamburger active">
             <span className="line line1"></span>
             <span className="line line2"></span>
             <span className="line line3"></span>
           </div>
         </div>
+
+        <div className="menu active">
+        <div className="menu-center">
+          <div className="menu-item">
+            <i className="fas fa-home"></i>
+            <a href="#intro" className="topbarA">About Me</a>
+          </div>
+          <div className="menu-item">
+            < a href="#portfolio" className="topbarA">Portfolio</a>
+          </div>
+          <div className="menu-item">
+          < a href="#works" className="topbarA">Works</a>
+          </div>
+          <div className="menu-item">
+          < a href="#resume" className="topbarA">Resume</a>
+          </div>
+          <div className="menu-item">
+          < a href="#contact" className="topbarA">Contact</a>
+          </div>
+        </div>
+        </div>
+
       </div>
     </div>
   );
